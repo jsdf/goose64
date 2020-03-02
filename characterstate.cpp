cd5aea255aae0ba1ad117418a8a5b98e0e69993a
James Friend <james@jsdf.co>
2020-03-01 23:24:12 -0500

    cpp link some files

--- characterstate.cpp
+++ characterstate.cpp
@@ -1,12 +1,1 @@
-
-#ifndef __N64__
-char* CharacterStateStrings[] = {
-    "IdleState",                //
-    "ConfusionState",           //
-    "DefaultActivityState",     //
-    "SeekingItemState",         //
-    "SeekingSoundSourceState",  //
-    "FleeingState",             //
-    "MAX_CHARACTER_STATE",      //
-};
-#endif
+./characterstate.c
--- modeltype.cpp
+++ modeltype.cpp
@@ -1,17 +1,1 @@
-
-#ifndef __N64__
-
-char* ModelTypeStrings[] = {
-    "NoneModel",                    //
-    "GooseModel",                   //
-    "BookItemModel",                //
-    "HomeworkItemModel",            //
-    "CakeItemModel",                //
-    "UniBldgModel",                 //
-    "UniFloorModel",                //
-    "BushModel",                    //
-    "FlagpoleModel",                //
-    "GroundskeeperCharacterModel",  //
-    "MAX_MODEL_TYPE",               //
-};
-#endif
+modeltype.c
