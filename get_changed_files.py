7968ab564817c4d261f5e547b5da21ed2f802ba1
James Friend <james@kitefire.com>
2020-03-24 23:54:11 -0400

    rebuild all when spec or makefile change

--- get_changed_files.py
+++ get_changed_files.py
@@ -15,11 +15,12 @@ except FileNotFoundError:
     pass
 
 c_files = glob.glob("*.c")
-h_files = glob.glob("*.h")
-
+full_rebuild_deps = glob.glob("*.h") + glob.glob("spec") + glob.glob("Makefile")
 
 c_files_mtimes = {filepath: os.path.getmtime(filepath) for filepath in c_files}
-h_files_mtimes = {filepath: os.path.getmtime(filepath) for filepath in h_files}
+full_rebuild_deps_mtimes = {
+    filepath: os.path.getmtime(filepath) for filepath in full_rebuild_deps
+}
 
 
 changed_files = []
@@ -29,14 +30,14 @@ for file, mtime in c_files_mtimes.items():
     if previous_mtimes.get(file, 0) < mtime:
         changed_files.append(file)
 
-# if any h file is changed, just bust the whole build cache
-for file, mtime in h_files_mtimes.items():
+# if any of these files are changed, just bust the whole build cache
+for file, mtime in full_rebuild_deps_mtimes.items():
     if previous_mtimes.get(file, 0) < mtime:
         changed_files = c_files
 
 if len(changed_files) > 0:
     print(" ".join(changed_files))
 
-updated_cache = {**c_files_mtimes, **h_files_mtimes}
+updated_cache = {**c_files_mtimes, **full_rebuild_deps_mtimes}
 with open(CACHE_FILE, "w") as outfile:
     json.dump(updated_cache, outfile)
