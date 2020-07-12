96b5a6abb98a452ba0a34b33056bcb39ed2fd53c
James Friend <james@kitefire.com>
2020-07-12 14:40:45 -0400

    more fixes for modern build

--- disassemble_modern.sh
+++ disassemble_modern.sh
@@ -8,8 +8,11 @@ set -eu
 pc_address="${1:-''}"
 
 
+# this file has original source code info but not final relocated symbol locations
 executable_file="codesegment.o"
 mips64-elf-objdump  --disassemble   --prefix-addresses  --source --wide "$executable_file" > "${executable_file}.dasm"
+# this file has final symbol locations but no source code. use offsets in functions to map instructions back to the codesegment.o file
+# would be good if spicy had add an option to not strip this stuff out
 executable_file="goose.out" # this should be the linked binary (not rom file)
 mips64-elf-objdump    --disassemble-all --source --prefix-addresses --wide --all-header "$executable_file" > "${executable_file}.dasm"
 
